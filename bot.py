1f10b9ae1399d1e946408d826a1c7ae28b82e4fb
KwakEuiJin <yuijin0909@gmail.com>
2024-04-11 18:14:43 +0900

    fix(everyone): Ignore if message tag everyone

--- bot.py
+++ bot.py
@@ -37,6 +37,9 @@ async def on_message(message:discord.Message):
 	# Ignore messages sent by the bot
 	if message.author == bot.user:
 		return
+	# Ignore messages sent to everyone
+	if '@everyone' in message.content:
+		return
 	# Check if the bot is mentioned or the message is a DM
 	if not (bot.user.mentioned_in(message) or isinstance(message.channel, discord.DMChannel) or message.channel.id in tracked_channels or message.channel.id in tracked_threads):
 		return
