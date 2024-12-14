34ad6b3bd92ae9a430216814fcdbd31962ccc974
Nikhil Raj Verma <darklacerta7817@gmail.com>
2024-12-14 20:14:30 +0530

    Docs and audio support

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import aiohttp
 import traceback
 from config import *
 from discord import app_commands
-from typing import Optional, Dict
+from typing import Optional, Dict, List
 import shelve
 
 #---------------------------------------------AI Configuration-------------------------------------------------
@@ -31,7 +31,7 @@ bot = commands.Bot(command_prefix=[], intents=intents,help_command=None,activity
 
 #On Message Function
 @bot.event
-async def on_message(message:discord.Message):
+async def on_message(message: discord.Message):
 	# Ignore messages sent by the bot
 	if message.author == bot.user:
 		return
@@ -41,62 +41,85 @@ async def on_message(message:discord.Message):
 	# Check if the bot is mentioned or the message is a DM
 	if not (bot.user.mentioned_in(message) or isinstance(message.channel, discord.DMChannel) or message.channel.id in tracked_channels or message.channel.id in tracked_threads):
 		return
-	#Start Typing to seem like something happened
+	# Start Typing to seem like something happened
 	try:
 		async with message.channel.typing():
 			print("FROM:" + str(message.author.name) + ": " + message.content)
 			query = ""
+			attachments = []
+
 			# Check if the message has attachments
-			images = []
 			if not message.attachments:
 				query = f"@{message.author.name} said \"{message.clean_content}\""
 			else:
 				# Check if empty message
 				if not message.content:
-					query = f"@{message.author.name} sent an image"
+					query = f"@{message.author.name} sent attachments:"
 				else:
-					query = f"@{message.author.name} said \"{message.clean_content}\" while sending an image"
-				
-				for attachment in message.attachments:
-					print("Attachment")
-					#these are the only image extentions it currently accepts
-					if any(attachment.filename.lower().endswith(ext) for ext in ['.png', '.jpg', '.jpeg', '.gif', '.webp']):
-						async with aiohttp.ClientSession() as session:
-							async with session.get(attachment.url) as resp:
-								if resp.status != 200:
-									await message.channel.send('Unable to download the image.')
-									return
-								image_data = await resp.read()
-								images.append({"mime_type": "image/jpeg", "data": image_data})
+					query = f"@{message.author.name} said \"{message.clean_content}\" while sending attachments:"
+				attachments = await get_attachment_data(message.attachments)
+				if not attachments:
+					await message.channel.send("An error occurred while processing your attachments.")
+					return
+				if len(attachments) == 0:
+					await message.channel.send("Attachments are of unsupported file types.")
+					return
 
 			# Check if message is quoting someone
 			if message.reference is not None:
 				reply_message = await message.channel.fetch_message(message.reference.message_id)
 				if reply_message.author.id != bot.user.id:
 					query = f"{query} while quoting @{reply_message.author.name} \"{reply_message.clean_content}\""
+				if reply_message.attachments:
+					attachments += await get_attachment_data(reply_message.attachments)
 
-			response_text = await generate_response(message.channel.id,images, query)
+			# Generate response using Gemini API
+			response_text = await generate_response(message.channel.id, attachments, query)
 
-		#Split the Message so discord does not get upset
-		await split_and_send_messages(message, response_text, 1700)
-		with shelve.open('chatdata') as file:
-			file[str(message.channel.id)] = message_history[message.channel.id].history
-		return
-		
+			await message.channel.send(response_text)
 	except Exception as e:
-		traceback.print_exc()
-		await message.reply('Some error has occurred, please check logs!')
+		print(f"Error: {e}")
+		print(traceback.format_exc())
+		if e.code == 50035:
+			await message.channel.send("The message is too long for me to process.")
+		else:
+			await message.channel.send('An error occurred while processing your message.')
 
+
+async def get_attachment_data(attachments:List[discord.Attachment]) -> List[Dict[str, bytes]]:
+	result = []
+	for attachment in attachments:
+		async with aiohttp.ClientSession() as session:
+			async with session.get(attachment.url) as resp:
+				if resp.status != 200:
+					return None
+				attachment_data = await resp.read()
+				mime_type = None
+				if any(attachment.filename.lower().endswith(ext) for ext in ['.png', '.jpeg', '.heic', '.webp', '.heif']):
+					mime_type = f"image/{attachment.filename.split('.')[-1]}"
+				elif any(attachment.filename.lower().endswith(ext) for ext in ['.jpg']):
+					mime_type = "image/jpeg"
+				elif any(attachment.filename.lower().endswith(ext) for ext in ['.wav', '.mp3', '.aiff', '.aac', '.ogg', '.flac']):
+					mime_type = f"audio/{attachment.filename.split('.')[-1]}"
+				elif any(attachment.filename.lower().endswith(ext) for ext in ['.html', '.css', '.md', '.csv', '.xml', '.rtf']):
+					mime_type = f"text/{attachment.filename.split('.')[-1]}"
+				elif any(attachment.filename.lower().endswith(ext) for ext in ['.pdf']):
+					mime_type = "application/pdf"
+				elif any(attachment.filename.lower().endswith(ext) for ext in ['.js']):
+					mime_type = "application/x-javascript"
+				elif any(attachment.filename.lower().endswith(ext) for ext in ['.py']):
+					mime_type = "application/x-python"
+				if mime_type:
+					result.append({"mime_type": mime_type, "data": attachment_data})
+	return result
 
 #---------------------------------------------AI Generation History-------------------------------------------------		   
 
-async def generate_response(channel_id,images,text):
+async def generate_response(channel_id,attachments,text):
 	try:
-		prompt_parts = images
+		prompt_parts = attachments
 		prompt_parts.append(text)
-		if not (channel_id in message_history):
-			message_history[channel_id] = model.start_chat(history=bot_template)
-		response = message_history[channel_id].send_message(prompt_parts)
+		response = await model.generate_content_async(prompt_parts)
 		return response.text
 	except Exception as e:
 		with open('errors.log','a+',encoding='utf-8') as errorlog:
